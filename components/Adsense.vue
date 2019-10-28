ad8b45288c29ec430210f0ce9609305ed7a2fc56
Chinaza Egbo <chinaza@zhaptek.com>
2019-10-28 00:11:40 +0000

    feat: add google adsense banners

---
+++ components/Adsense.vue
@@ -0,0 +1,23 @@
+<template>
+  <div className="ad" style="width:100%">
+    <ins
+      class="adsbygoogle"
+      style="display: block"
+      data-ad-format="fluid"
+      data-ad-layout-key="-fb+5w+4e-db+86"
+      data-ad-client="ca-pub-8522455793322408"
+      data-ad-slot="8376613461"
+    />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Adsense'
+};
+</script>
+<style scoped>
+.box {
+  margin-bottom: 10px;
+}
+</style>
--- pages/index/index.vue
+++ pages/index/index.vue
@@ -1,23 +1,32 @@
 <template>
   <div class="home">
     <h6 style="margin: 0 auto; margin-bottom:5px">RECENT TWEETS</h6>
-    <Gig v-for="(tweet, i) in tweets" :key="i" :tweet="tweet" />
+    <div v-for="(tweet, i) in tweets" :key="i">
+      <Gig :tweet="tweet" />
+      <!-- <Adsense v-if="i % 4 === 0 && i !== 0" /> -->
+    </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import Gig from '~/components/Gig.vue';
+// import Adsense from '~/components/Adsense.vue';
 
 export default {
   name: 'Home',
   components: {
     Gig
+    // Adsense
   },
   computed: {
     tweets() {
       return this.$store.state.twitter.tweets;
     }
+  },
+  mounted() {
+    const gwindow = window;
+    (gwindow.adsbygoogle = gwindow.adsbygoogle || []).push({});
   },
   head() {
     return {
