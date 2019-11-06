b11c278dede719973e3514351eb161db05870652
Chinaza Egbo <chinaza@zhaptek.com>
2019-11-06 02:39:23 +0000

    feat: integrate live chat

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,6 +1,21 @@
 <template>
   <div>
     <!-- Global site tag (gtag.js) - Google Analytics -->
+    <!--Start of Tawk.to Script-->
+    <script type="text/javascript">
+      var Tawk_API = Tawk_API || {},
+        Tawk_LoadStart = new Date();
+      (function() {
+        var s1 = document.createElement('script'),
+          s0 = document.getElementsByTagName('script')[0];
+        s1.async = true;
+        s1.src = 'https://embed.tawk.to/5dc23133e4c2fa4b6bda3029/default';
+        s1.charset = 'UTF-8';
+        s1.setAttribute('crossorigin', '*');
+        s0.parentNode.insertBefore(s1, s0);
+      })();
+    </script>
+    <!--End of Tawk.to Script-->
     <script
       async
       src="https://www.googletagmanager.com/gtag/js?id=G-4G8HR0XV2V"
