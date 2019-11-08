54e6b2297c3a25a18bf7fa63b5b6245f139b3692
Chinaza Egbo <chinaza@zhaptek.com>
2019-11-08 21:31:56 +0000

    fix: import script on each ad

--- components/Adsense.vue
+++ components/Adsense.vue
@@ -1,5 +1,9 @@
 <template>
   <div class="box constrained-box tw-card" style="width:100%">
+    <script
+      async
+      src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"
+    ></script>
     <ins
       class="adsbygoogle"
       style="display:block"
@@ -8,6 +12,9 @@
       data-ad-client="ca-pub-8522455793322408"
       data-ad-slot="2134179299"
     ></ins>
+    <script>
+      (adsbygoogle = window.adsbygoogle || []).push({});
+    </script>
   </div>
 </template>
 
