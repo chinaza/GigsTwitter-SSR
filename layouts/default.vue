1ad944e12b32931fd1a32d48ac5c822c08a2b38f
Chinaza Egbo <chinaza@zhaptek.com>
2019-11-08 23:32:25 +0000

    fix analytics

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,37 +1,5 @@
 <template>
   <div>
-    <!--Start of Tawk.to Script-->
-    <script type="text/javascript">
-      var Tawk_API = Tawk_API || {},
-        Tawk_LoadStart = new Date();
-      (function() {
-        var s1 = document.createElement('script'),
-          s0 = document.getElementsByTagName('script')[0];
-        s1.async = true;
-        s1.src = 'https://embed.tawk.to/5dc23133e4c2fa4b6bda3029/default';
-        s1.charset = 'UTF-8';
-        s1.setAttribute('crossorigin', '*');
-        s0.parentNode.insertBefore(s1, s0);
-      })();
-    </script>
-    <!--End of Tawk.to Script-->
-    <!-- Global site tag (gtag.js) - Google Analytics -->
-    <script
-      async
-      src="https://www.googletagmanager.com/gtag/js?id=G-4G8HR0XV2V"
-    ></script>
-    <script>
-      window.dataLayer = window.dataLayer || [];
-
-      function gtag() {
-        dataLayer.push(arguments);
-      }
-      gtag('js', new Date());
-
-      gtag('config', 'G-4G8HR0XV2V');
-      window.gtag = gtag;
-    </script>
-    <!-- End Google Analytics -->
     <nuxt />
   </div>
 </template>
